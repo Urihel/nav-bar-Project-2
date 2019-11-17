b487f8cab6e36c82ac19b98d798762c02d6f2b9e
Urihel Coleman <45701213+Urihel@users.noreply.github.com>
2019-11-16 16:42:04 -0800

    Update navStyle.js

--- navStyle.js
+++ navStyle.js
@@ -1,3 +1,10 @@
-//javascript file for navbar project
-var logoname = document.getElementById('logoName');
-logoname.style.color = "red";
+var logo = document.getElementById("logoName");
+//code below is used to add audio and play the audio when the logo is clicked. 
+logo.addEventListener("click", function () {
+    soundeffect1 = document.getElementById("sound1");
+        if(true) {
+            soundeffect1.play();
+        }
+});
+
+
